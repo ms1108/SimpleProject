6114ea2972ef2d6368bdca3b95595c0bc320322e
mosheng <929262660@qq.com>
2024-01-18 14:40:38 +0800

    jar input

--- src/main/java/com/ms/controller/Controller.java
+++ src/main/java/com/ms/controller/Controller.java
@@ -1,9 +1,20 @@
 package com.ms.controller;
 
+import User.JarTest;
+import User.JarTestBase;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 public class Controller {
@@ -27,5 +38,40 @@ public class Controller {
     public String nginx() {
         System.out.println("hello");
         return "hello";
+    }
+
+    //@GetMapping("/getJar")
+    public static void main(String[] args) {
+        //JarTest jarTest = new JarTest();
+        //System.out.println(jarTest.getName());
+        try {
+            String json = "{\"name\":\"John\"}";
+            ClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new URL("file:" + "/lib/jarTest-1.0-SNAPSHOT.jar.jar")}, Controller.class.getClassLoader());
+            Class<JarTestBase> aClass = (Class<JarTestBase>) Class.forName("User.JarTest", true, classLoader);
+            Gson gson = new Gson();
+            JarTestBase jarTestBase = gson.fromJson(json, aClass);
+            jarTestBase.handle();
+            System.out.println(jarTestBase);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/getJar")
+    public String getJar() {
+        String handle = "";
+        try {
+            String json = "{\"name\":\"John\"}";
+            ClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new URL("file:" + "/lib/jarTest-1.0-SNAPSHOT.jar.jar")}, Controller.class.getClassLoader());
+            Class<JarTestBase> aClass = (Class<JarTestBase>) Class.forName("User.JarTest", true, classLoader);
+            Gson gson = new Gson();
+            JarTestBase jarTestBase = gson.fromJson(json, aClass);
+            handle = jarTestBase.handle();
+            System.out.println(jarTestBase);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return handle;
     }
 }
